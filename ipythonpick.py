efaf1b81d08c9650b6971a8b9ab7a4afce5c0a10
Marcio Mazza <marciomazza@gmail.com>
2015-06-28 19:05:17 -0300

    Add path search for ipython_log.py files

--- ipythonpick.py
+++ ipythonpick.py
@@ -1,25 +1,42 @@
-import os
+import inspect
+from os import path
+
 import sublime
 import sublime_plugin
-import inspect
 
 
-LIMIT = 3
+LIMIT = 20
+IPYLOG = 'ipython_log.py'
 
 
 class IpythonPickCommand(sublime_plugin.TextCommand):
 
-    def run(self, edit):
-
+    def search_folders(self):
+        # project folders
         project_data = sublime.active_window().project_data()
-        for folder in project_data['folders']:
-            ipython_log_path = os.path.join(folder['path'], 'ipython_log.py')
-            if os.path.exists(ipython_log_path):
-                with open(ipython_log_path, 'r') as f:
-                    options = [l.strip() for l in f.readlines()[-LIMIT:]]
+        if project_data:
+            for folder in project_data.get('folders', []):
+                yield folder['path']
+        # climb path
+        fname = self.view.fname()
+        while True:
+            folder = path.dirname(fname)
+            if fname == folder:
+                break  # hit file system root
+            else:
+                yield folder
+                fname = folder
+
+    def run(self, edit):
+        for folder in self.search_folders():
+            ipylog = path.join(folder, 'ipython_log.py')
+            if path.exists(ipylog):
                 break
-        else:
-            options = []
+            else:
+                ipylog = None
+
+        with open(ipylog, 'r') as f:
+            options = [l.strip() for l in f.readlines()[-LIMIT:]]
 
         def sel(index):
             text = options[index]
